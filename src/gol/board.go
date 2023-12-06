f8bd0a259b8c0d642fcf270e68a42ca8e0445f9f
ftambara <98750668+ftambara@users.noreply.github.com>
2023-12-06 19:20:39 -0300

    Use width and height words

--- src/gol/board.go
+++ src/gol/board.go
@@ -12,14 +12,20 @@ type Board struct {
 }
 
 func NewBoard(xSize, ySize int, fn func(int, int) CellState) *Board {
-	states := make([][]CellState, ySize)
-	underlying := make([]CellState, xSize*ySize)
+	// Use padding to avoid bounds checking
+	realXSize := xSize + 2
+	realYSize := ySize + 2
+	states := make([][]CellState, realYSize)
+	underlying := make([]CellState, realXSize*realYSize)
 	for y := range states {
-		states[y], underlying = underlying[:xSize], underlying[xSize:]
+		states[y], underlying = underlying[:realXSize], underlying[realXSize:]
 	}
 	for y := range states {
 		for x := range states[y] {
-			states[y][x] = fn(x, y)
+			if x == 0 || x == realXSize-1 || y == 0 || y == realYSize-1 {
+				continue
+			}
+			states[y][x] = fn(x-1, y-1)
 		}
 	}
 	return &Board{states, xSize, ySize}
@@ -35,29 +41,24 @@ func RandomBoard(xSize, ySize int) *Board {
 }
 
 func (b *Board) at(x, y int) CellState {
-	return b.states[y][x]
+	return b.states[y+1][x+1]
 }
 
 func (b *Board) set(x, y int, s CellState) {
-	b.states[y][x] = s
+	b.states[y+1][x+1] = s
 }
 
 func (b *Board) advance() *Board {
-	nextBoard := NewBoard(b.width, b.height, func(_, _ int) CellState {
-		return Off
+	nextBoard := NewBoard(b.width, b.height, func(x, y int) CellState {
+		count := b.CountNeighbors(x, y)
+		return next(b.at(x, y), count)
 	})
-	for y := range b.states {
-		for x := range b.states[y] {
-			count := b.CountNeighbors(x, y)
-			nextBoard.set(x, y, next(b.at(x, y), count))
-		}
-	}
 	return nextBoard
 }
 
 func (b *Board) print() {
-	for y := range b.states {
-		for x := range b.states[y] {
+	for y := 0; y < b.height; y++ {
+		for x := 0; x < b.width; x++ {
 			fmt.Printf("%v  ", b.at(x, y))
 		}
 		fmt.Print("\n\n")
@@ -66,14 +67,12 @@ func (b *Board) print() {
 
 func (b *Board) CountNeighbors(x, y int) int {
 	count := 0
-	// Bound r and c with board edges
-	for xx := max(x-1, 0); xx <= min(x+1, b.width-1); xx++ {
-		for yy := max(y-1, 0); yy <= min(y+1, b.height-1); yy++ {
-			if xx == x && yy == y {
-				// Given cell, skip
+	for dy := -1; dy <= 1; dy++ {
+		for dx := -1; dx <= 1; dx++ {
+			if dx == 0 && dy == 0 {
 				continue
 			}
-			if b.at(xx, yy) == On {
+			if b.at(x+dx, y+dy) == On {
 				count++
 			}
 		}
---
+++ src/gol/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"runtime/pprof"
+	"time"
+)
+
+var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to file")
+
+func main() {
+	flag.Parse()
+	if *cpuprofile != "" {
+		f, err := os.Create(*cpuprofile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		pprof.StartCPUProfile(f)
+		defer pprof.StopCPUProfile()
+	}
+	board := RandomBoard(1000, 1000)
+	for i := 0; i < 20; i++ {
+		nextBoard := board.advance()
+		board = nextBoard
+	}
+}
+
+func timer(name string) func() {
+	start := time.Now()
+	return func() {
+		fmt.Printf("%s took %v\n", name, time.Since(start))
+	}
+}
